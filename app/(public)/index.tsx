86ce28f74ddb6176e9fd81687fedcc9885180940
Harshal997 <harshalkher27@gmail.com>
2026-01-15 18:48:01 +0530

    feat: add routes

---
+++ app/(public)/index.tsx
@@ -0,0 +1,7 @@
+import { SignIn } from "@/components/clerk/SignIn";
+
+export default function Index() {
+  return (
+    <SignIn scheme="ciana" signUpUrl="" />
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,36 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) {
+    return <View><Text>Loading...</Text></View>;
+  }
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(protected)" />
+      </Stack.Protected>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(public)" />
+      </Stack.Protected>
+    </Stack>
+  );
+}
 
 export default function RootLayout() {
-  return <Stack screenOptions={{
-    headerShown: false
-  }} />;
+  return (
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <RootLayoutWithAuth />
+    </ClerkProvider>
+  );
 }
